5f95724b3fcf6b693e54a889bea03c547886753a
jonsirrine <sir15004@byui.edu>
2018-07-23 13:26:39 -0700

    added background

---
+++ jons_collection/CANVAS/models/Game.js
@@ -0,0 +1,12 @@
+function Game(){
+    this.width = 1000;
+    this.height = 800;
+    this.canvas = {
+        width: 800,
+        height: 600
+    };
+    this.backgroundColor = "#91ff7c";
+    this.backgroundImage = null;
+    this.players = {};
+    this.baddies = [];
+}
--- jons_collection/CANVAS/models/Robot.js
+++ jons_collection/CANVAS/models/Robot.js
@@ -7,25 +7,15 @@ function Robot(x, y, size, speed, color, game){
     this.speed = speed;
     this.color = color;
     this.game = game;
-    this.hits = 0;
-    this.delay = 0;
-    this.startDelay = false;
-    this.hasSplit = false;
 }
 Robot.prototype.updatePos = function(){
     this.x += this.dx;
     this.y += this.dy;
     if ((this.x + this.size) > this.game.width || this.x < 0){
         this.dx = -this.dx;
-        this.hits++;
     }
-    if (this.y + this.size > this.game.height || this.y < 0){
+    if (this.y + this.size > this.game.height || this.y < 0) {
         this.dy = -this.dy;
-        this.hits++;
-    }
-    if (this.hits >= 3 && !this.hasSplit){
-        //this.duplicate();
-        this.hasSplit = true;
     }
 };
 
@@ -33,9 +23,4 @@ Robot.prototype.draw = function(){
     stroke(0);
     fill(this.color);
     rect(this.x, this.y, this.size, this.size);
-};
-
-Robot.prototype.duplicate = function(){
-    let robot = new Robot(this.x, this.y, this.size, this.speed, 50, Game);
-    this.game.baddies.push(robot);
 };
